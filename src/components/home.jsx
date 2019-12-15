c7f09e876d6e5da6d493a672d1d36100ccca4ae3
WStrellis <wstrellis@gmail.com>
2019-12-15 18:51:28 -0500

    removed env variable showing in DOM

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,9 +3,6 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <p>The auth domain is: {process.env.REACT_APP_AUTH0_DOMAIN}</p>
-      <p>The auth client id is: {process.env.REACT_APP_AUTH0_CLIENT_ID}</p>
-      <p>the NODE_ENV is: {process.env.NODE_ENV}</p>
       <title>Home</title>
       <h1>Welcome</h1>
       <img src='https://media.giphy.com/media/h8mSIeTWzDFooj3hgT/giphy.gif' />
