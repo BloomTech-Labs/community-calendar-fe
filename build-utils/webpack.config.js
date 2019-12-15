9f0f02e2c90e8b97042974d2b59d92dfa7f511fa
WStrellis <wstrellis@gmail.com>
2019-12-15 16:11:25 -0500

    trying stuff

--- build-utils/webpack.config.js
+++ build-utils/webpack.config.js
@@ -6,6 +6,7 @@ const path = require('path')
 const configEnvKeys = require('./env_variables.config.js')
 
 module.exports = env => {
+  console.log('process.env.AUTH0_DOMAIN', process.env.AUTH0_DOMAIN)
   console.log('command line env variables', env)
   const isDevelopment = env.environment === 'development'
   // environment specific settings
