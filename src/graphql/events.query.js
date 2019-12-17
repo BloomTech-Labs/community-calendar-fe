98818186c737ecf6a953d9e9ee26171e7c42243d
gelinas <jlgelinas@gmail.com>
2019-12-17 13:27:22 -0500

    query and query fragments for event data

---
+++ src/graphql/events.query.js
@@ -0,0 +1,48 @@
+import gql from 'graphql-tag'
+
+export const EVENT_DETAIL_DATA = gql`
+  fragment EventDetail on Event {
+    id
+    title
+    description
+    start
+    end
+  }
+`;
+
+export const ADDRESS_DETAIL_DATA = gql`
+  fragment AddressDetail on Location {
+    street_address
+    street_address_2
+    city
+    zipcode
+    state
+  }
+`;
+
+export const GET_EVENTS = gql`
+  query {
+    events {
+      ...EventDetail
+      creator {
+        id
+      }
+      locations {
+        id
+        name
+        ...AddressDetail
+      }
+    }
+  }
+  ${EVENT_DETAIL_DATA}
+  ${ADDRESS_DETAIL_DATA}
+`;
+
+export const TEST_GET = gql`
+  query {
+    events {
+      ...EventDetail
+    }
+  }
+  ${EVENT_DETAIL_DATA}
+`;
