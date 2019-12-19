5010bc088324e12f2934692fb848d75e5a02dfe5
lowell1 <lowelljacobs@mail.com>
2019-12-19 15:53:29 -0800

    added grey background color when hovering over EventListCard

--- src/components/events/EventView.jsx
+++ src/components/events/EventView.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import banner from '../../assets/images/bannerevent.png'
+
 const EventView = props => {
-    console.log(props)
+    const eventData = props.location.state.item;
+
+    console.log(eventData);
     return (
         <div className='mainContainer'>
             <h1>Event View Works</h1>
             <div className='subContainer'>
                 <img src={banner} alt='banner' />
             </div>
-
         </div>
-    )
+    );
 }
 
 export default EventView
