f1d07fbf494bff089d6c695f482aca7bda69d786
lowell1 <lowelljacobs@mail.com>
2019-12-20 00:41:14 -0800

    created new div with event information

--- src/components/events/EventView.jsx
+++ src/components/events/EventView.jsx
@@ -3,10 +3,13 @@ import React from "react";
 //styles
 import {
     banner,
-    header,
+    top_div,
+    middle_div,
+    bottom_div,
     container,
     panel_left,
     panel_right,
+    space_around,
     main
 } from "../style_modules/EventView.module.scss";
 
@@ -24,7 +27,7 @@ const EventView = props => {
         event_images
     } = props.location.state.item;
 
-    console.log(locations);
+    console.log(props.location.state.item);
 
     //destructure first item in locations array (why is it an array?)
     const {
@@ -37,7 +40,17 @@ const EventView = props => {
     } = locations[0];
 
     //convert start date to Date object
-    const start_date = new Date(start);
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    //create string for displaying event time in hours and minutes
+    const startHours = startDate.getHours();
+    const startMinutes = startDate.getMinutes();
+    const eventStartTime = startHours > 12 ? `${startHours - 12}:${String(startMinutes).padStart(2, "0")} pm` : `${startHours}:${String(startMinutes).padStart(2, "0")} am`
+
+    const endHours = endDate.getHours();
+    const endMinutes = endDate.getMinutes();
+    const eventEndTime = endHours > 12 ? `${endHours - 12}:${String(endMinutes).padStart(2, "0")} pm` : `${endHours}:${String(endMinutes).padStart(2, "0")} am`
 
     //month names for getting the name of the month (date object return index)
     var months = [
@@ -68,10 +81,10 @@ const EventView = props => {
     return (
         <div className={`${container} Butler`}>
             <img className={banner} src={event_images[0].url} alt="banner" />
-            <div className={header}>
+            <div className={top_div}>
                 <div className={panel_left}>
                     <h1>{title}</h1>
-                    <p>{`${months[start_date.getMonth()]} ${start_date.getDate()}, ${start_date.getFullYear()} ${weekDays[start_date.getDay()]}`}</p>
+                    <p>{`${months[startDate.getMonth()]} ${startDate.getDate()}, ${startDate.getFullYear()} ${weekDays[startDate.getDay()]}`}</p>
                     <p>{`${street_address}, ${name}`}</p>
                 </div>
                 <div className={panel_right}>
@@ -88,7 +101,22 @@ const EventView = props => {
                     </p>
                 </div>
             </div>
-            <div className={main}>
+            <div className={middle_div}>
+                <div>
+                    <p className="color_chalice">Hosted by:</p>
+                    {/* <p className="color_shark">{creator}</p> */}
+                </div>
+                <div>
+                    <p className="color_chalice">Time:</p>
+                    <p className="color_shark">{`${eventStartTime} - ${eventEndTime}`}</p>
+                </div>
+                <div>
+                    <p className="color_chalice">Ticket type:</p>
+                    <p className="color_shark"></p>
+                </div>
+                <button>Follow Host</button>
+            </div>
+            <div className={bottom_div}>
                 <div>
                     
                 </div>
