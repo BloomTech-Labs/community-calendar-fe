e53793734f80a288f5fbe871165e187477e145a5
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-01-06 16:36:04 -0600

    Adds basic carousel feature

--- src/components/featured/FeaturedCarousel.jsx
+++ src/components/featured/FeaturedCarousel.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React from "react";
 
+import ReactSimpleCarousel from "react-spring-carousel";
 
-const Carousel = () => {
+const Image = 'https://detroitmi.gov/sites/detroitmi.localhost/files/2018-11/Mary-Sheffield.jpg';
+
+const slides = [
+  {
+    image: Image,
+    text: "Exercitation tempor"
+  },
+  {
+    image: Image,
+    text: "dolore proident id"
+  },
+  {
+    image: Image,
+    text: "proident id irure"
+  },
+  {
+    image: Image,
+    text: "Exercitation irure"
+  },
+  {
+    image: Image,
+    text: "dolore proident id irure"
+  }
+];
+function Carousel() {
   return (
-    <div>
-      <p>Carousel</p>
+    <div className="App">
+      <ReactSimpleCarousel slidesToShow={5}>
+        {slides.map(({ image, text }, i) => (
+          <div key={i} className="App-slide">
+            <a href={`#${i}`} draggable="false">
+              <img src={image} alt="" />
+            </a>
+            <p
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translateX(-50%) translateY(-50%)"
+              }}
+            >
+              {text}
+            </p>
+          </div>
+        ))}
+      </ReactSimpleCarousel>
     </div>
-  )
+  );
 }
 
 export default Carousel;
