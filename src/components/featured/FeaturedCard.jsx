a72cba45be1d40815057503456c9468131004a63
lowell1 <lowelljacobs@mail.com>
2020-01-07 16:05:13 -0800

    moved carousel dot buttons down, fixed padding/margin around carousel
    Co-authored-by: Westley-Strellis <wstrellis@gmail.com>

--- src/components/featured/FeaturedCard.jsx
+++ src/components/featured/FeaturedCard.jsx
@@ -14,6 +14,7 @@ import {
 Cards for the Featured Events carousel.
 */
 
+
 export default function FeaturedCard(props) {
   const {item} = props
 
--- src/components/featured/FeaturedCarousel.jsx
+++ src/components/featured/FeaturedCarousel.jsx
@@ -12,8 +12,9 @@ import {featuredWrapper} from './Featured.module.scss'
 import ReactSimpleCarousel from "react-spring-carousel";
 
 function FeatCarousel({apolloData: {data, loading, error}}) {
+
   return (
-    <section className='section'>
+    <section style={{paddingBottom: 0}} className='section'>
       <h3 className='is-family-secondary is-size-2 has-text-black-bis'>
         Featured
       </h3>
