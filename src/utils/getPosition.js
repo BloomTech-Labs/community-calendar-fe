ab8fa04cee2b996865e1b21ab6434393d837f74a
WStrellis <wstrellis@gmail.com>
2020-01-10 08:08:30 -0500

    started writing custom geolocation hook

---
+++ src/utils/getPosition.js
@@ -0,0 +1,45 @@
+import {useState, useEffect} from 'react'
+
+/* 
+ This function is used to read the user's geolocation coordinates. When a component utilizing this function 
+ is mounted the user is prompted if the app can use their current location. 
+  */
+
+export default function getGeoPosition(options) {
+  const [userPosition, setUserPosition] = useState({})
+
+  // config settings. Args overide defaults
+  options = {
+    enableHighAccuracy: !options.enableHighAccuracy
+      ? options.enableHighAccuracy
+      : true,
+    // execute error function if getCurrentPosition does not resolve within 5 seconds
+    timeout: options.timeout || 5000,
+    //maximum time to cache results
+    maximumAge: options.maximumAge || 0,
+  }
+
+  // function called if user's position has been obtained
+  const onSuccess = pos => {
+    console.log('User coordinates', pos.coords)
+    // set the userPosition
+    setUserPosition(pos.coords)
+  }
+
+  // function called if user does not allow access or an error occurs
+  const onError = err => {
+    console.warn(`getGeoPosition ERROR ${err.code}: ${err.message}`)
+  }
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      window.navigator.geolocation.getCurrentPosition(
+        onSuccess,
+        onError,
+        options,
+      )
+    }
+  }, [])
+
+  return [userPosition, setUserPosition]
+}
