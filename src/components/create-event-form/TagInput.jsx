5101ef29a13e7f07dd796a3135faa656d85dc792
lowell1 <lowelljacobs@mail.com>
2020-01-16 14:52:31 -0800

    moved helper functions to seperate component

--- src/components/create-event-form/TagInput.jsx
+++ src/components/create-event-form/TagInput.jsx
@@ -10,51 +10,40 @@ import {
 import {useQuery} from '@apollo/react-hooks'
 import {GET_ALL_TAGS} from '../../graphql/events.query'
 
-const tagSorter = (str1, str2) => {
-    const shorterStrLen = str1.length < str2.length ? str1.length : str2.length;
-  
-    for(let i = 0; i < shorterStrLen; i++) {
-      if(str1[i] !== str2[i]) {
-        return str1[i] < str2[i] ? 1 : -1
-      }
-    }
-    return 0;
-  }
 
-const TagInput = ({selectedTags, setSelectedTags}) => {
+// const TagInput = React.forwardRef(({
+//     // filterTags,
+//     handleKeyDown,
+//     handleClick,
+//     removeSelectedTag,
+//     addSelectedTag,
+//     // findSelectedTagIndex,
+//     tagSorter,
+//     selectedTags,
+//     hasFocus,
+//     tagDisplay    
+// }) => {
+const TagInput = React.forwardRef((props, tagDisplay) => {
+
+    const {
+            // filterTags,
+            handleKeyDown,
+            handleClick,
+            removeSelectedTag,
+            addSelectedTag,
+            // findSelectedTagIndex,
+            tagSorter,
+            selectedTags,
+            hasFocus,
+        } = props
+
     const {data, loading, error} = useQuery(GET_ALL_TAGS);
 
-    const [hasFocus, setHasFocus] = useState();
-    const tagDisplay = useRef(null);
-
-    const findSelectedTagIndex = tagId => {
-        return selectedTags.findIndex(selectedTag => {
-            return tagId === selectedTag.id;
-        });
-    }
-    
-    const addSelectedTag = e => {
-        //tag has not already been selected
-        const newTag = {title: e.target.getAttribute("tag_title"), id: e.target.getAttribute("tag_id")};
-        // const newTag = {title: e.target.innerHTML, id: e.target.getAttribute("tag_id")};
-
-        if(findSelectedTagIndex(newTag.id) === -1) 
-            setSelectedTags([...selectedTags, newTag]);
-    }
-
-    const removeSelectedTag = tagId => {
-        const index = findSelectedTagIndex(tagId);
-        setSelectedTags([...selectedTags.slice(0, index), ...selectedTags.slice(index + 1)]);
-    }
-        
-    const handleClick = event => {
-        setHasFocus(tagDisplay.current.contains(event.target));
-    }; 
-        
     useEffect(() => {
         
         //componentDidMount
         document.addEventListener("click", handleClick);
+
         
         //componentWillUnmount
         return () => {
@@ -70,10 +59,10 @@ const TagInput = ({selectedTags, setSelectedTags}) => {
         return <div>Error loading tags</div>
     }
 
-    data.tags = data.tags.sort(tagSorter);
+    // data.tags = data.tags.sort(tagSorter);
 
      return (
-        <div>
+        <div onKeyDown={handleKeyDown} tabIndex="0">
             <div className={tagDisplayClass} ref={tagDisplay}>
                 {                    
                     selectedTags.length === 0 
@@ -92,7 +81,9 @@ const TagInput = ({selectedTags, setSelectedTags}) => {
                 (
                     <div className={tagList}>
                         {
-                            data.tags.map((tag, idx) => (
+                            data.tags//.filter(filterTags)
+                            .sort(tagSorter)
+                            .map((tag, idx) => (
                                     <p 
                                         key={tag.id} 
                                         tag_id={tag.id}
@@ -108,6 +99,85 @@ const TagInput = ({selectedTags, setSelectedTags}) => {
             }
         </div>
     );
-};
+});
 
-export default TagInput;
+const TagInputHelpers = ({selectedTags, setSelectedTags}) => {
+    const tagSorter = (tag1, tag2) => {
+        const str1 = tag1.title.toLowerCase();
+        const str2 = tag2.title.toLowerCase();
+        const shorterStrLen = str1.length < str2.length ? str1.length : str2.length;
+      
+        for(let i = 0; i < shorterStrLen; i++) {
+          if(str1[i] !== str2[i]) {
+            return str1[i] < str2[i] ? -1 : 1
+          }
+        }
+        return str1.length < str2.length ? -1 : 1;
+    }
+    
+    const findSelectedTagIndex = tagId => {
+        return selectedTags.findIndex(selectedTag => {
+            return tagId === selectedTag.id;
+        });
+    }
+
+    const [hasFocus, setHasFocus] = useState();
+
+    //filter tags based on what user types
+    //clear tagSearch when user clicks off TagInput
+    const [tagSearch, setTagSearch] = useState();
+
+    const tagDisplay = useRef(null);
+    
+    const addSelectedTag = e => {
+        //tag has not already been selected
+        const newTag = {title: e.target.getAttribute("tag_title"), id: e.target.getAttribute("tag_id")};
+        // const newTag = {title: e.target.innerHTML, id: e.target.getAttribute("tag_id")};
+
+        if(findSelectedTagIndex(newTag.id) === -1) 
+            setSelectedTags([...selectedTags, newTag]);
+    }
+
+    const removeSelectedTag = tagId => {
+        const index = findSelectedTagIndex(tagId);
+        setSelectedTags([...selectedTags.slice(0, index), ...selectedTags.slice(index + 1)]);
+    }
+        
+    const handleClick = event => {
+        setHasFocus(tagDisplay.current.contains(event.target));
+    };
+
+    const handleKeyDown = event => {
+        console.log(hasFocus);
+        if(hasFocus) {
+            setTagSearch(tagSearch + event.key);
+            console.log(tagSearch);
+        }
+    };
+
+    const filterTags = tag => {
+        if(tagSearch.length === 0)
+            return true;
+
+        return tag.title.toLowerCase().contains(tagSearch.toLowerCase());
+    };
+    
+    return (
+        <TagInput 
+            filterTags={filterTags} 
+            handleKeyDown={handleKeyDown}
+            handleClick={handleClick}
+            removeSelectedTag={removeSelectedTag}
+            addSelectedTag={addSelectedTag}
+            findSelectedTagIndex={findSelectedTagIndex}
+            tagSorter={tagSorter}
+            tagSearch={tagSearch}
+            selectedTags={selectedTags}
+            setSelectedTags={setSelectedTags}
+            hasFocus={hasFocus}
+            ref={tagDisplay}
+        />
+    )
+}
+
+export default TagInputHelpers;
