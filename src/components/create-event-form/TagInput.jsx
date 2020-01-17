30f645d2ad414c558b1ac4e0678af06f356f3872
lowell1 <lowelljacobs@mail.com>
2020-01-16 23:45:43 -0800

    redid tag component with input field for searching

--- src/components/create-event-form/TagInput.jsx
+++ src/components/create-event-form/TagInput.jsx
@@ -1,8 +1,13 @@
+/**
+ * Enter should add first tag in list to the selected tags
+ * down/up arrow should select last/next item in selectable tags
+ * keydown handler should ignore weird keystrokes?
+ */
 import React, {useState, useRef, useEffect} from "react";
 
 import {
     tagDisplayClass,
-    tagList,
+    suggestedTags,
     tagClass
 } from "./styles/TagInput.module.scss"
 
@@ -10,36 +15,95 @@ import {
 import {useQuery} from '@apollo/react-hooks'
 import {GET_ALL_TAGS} from '../../graphql/events.query'
 
-
-// const TagInput = React.forwardRef(({
-//     // filterTags,
-//     handleKeyDown,
-//     handleClick,
-//     removeSelectedTag,
-//     addSelectedTag,
-//     // findSelectedTagIndex,
-//     tagSorter,
-//     selectedTags,
-//     hasFocus,
-//     tagDisplay    
-// }) => {
 const TagInput = React.forwardRef((props, tagDisplay) => {
 
-    const {
-            filterTags,
-            handleKeyDown,
-            handleClick,
-            removeSelectedTag,
-            addSelectedTag,
-            // findSelectedTagIndex,
-            tagSorter,
-            selectedTags,
-            hasFocus,
-            tagSearch,
-            setTagSearch
-        } = props
-
     const {data, loading, error} = useQuery(GET_ALL_TAGS);
+
+    const [selectedTags, setSelectedTags] = useState([]);
+
+    //input field for searching tags
+    const [tagInput, setTagInput] = useState("");
+
+    //ref to element for displaying suggested tags to toggle its visibility
+    const suggestedTagsRef = useRef(null);
+
+    //ref for tagInput to check if it is focused
+    const tagInputRef = useRef(null);
+
+    //handler for when user types in tag input box for searching tags
+    const handleTagInputChange = event => {
+        setTagInput(event.target.value);
+    }
+    console.log(tagInput);
+
+    const filterTags = tag => {
+        if(tagInput.length === 0)
+            return true;
+
+        return new RegExp(`\\b${tagInput}`, "i").test(tag.title);
+    };
+
+    const tagSorter = (tag1, tag2) => {
+        const str1 = tag1.title.toLowerCase();
+        const str2 = tag2.title.toLowerCase();
+        const shorterStrLen = str1.length < str2.length ? str1.length : str2.length;
+      
+        for(let i = 0; i < shorterStrLen; i++) {
+          if(str1[i] !== str2[i]) {
+            return str1[i] < str2[i] ? -1 : 1
+          }
+        }
+        return str1.length < str2.length ? -1 : 1;
+    }
+
+    const findSelectedTagIndex = tagId => {
+        return selectedTags.findIndex(selectedTag => {
+            return tagId === selectedTag.id;
+        });
+    }
+
+    const addSelectedTag = tag => {
+        // const newTag = {title: e.target.getAttribute("tag_title"), id: e.target.getAttribute("tag_id")};
+        console.log(tag);
+        
+        //tag has not already been selected
+        if(findSelectedTagIndex(tag.id) === -1) 
+            setSelectedTags([...selectedTags, tag]);
+    }
+
+    const removeSelectedTag = tagId => {
+        const index = findSelectedTagIndex(tagId);
+        setSelectedTags([...selectedTags.slice(0, index), ...selectedTags.slice(index + 1)]);
+    }
+
+    const hideTags = e => {
+        suggestedTagsRef.current.style.visibility = "hidden";
+    }
+    
+    const showTags = () => {
+        suggestedTagsRef.current.style.visibility = "visible";
+    }
+        
+    const handleClick = event => {
+        if(tagInputRef.current.contains(event.target)) {
+            showTags()
+        } else {
+            setTagInput("");
+            hideTags();
+        } 
+    };
+
+    
+    
+    
+
+    // const handleKeyDown = event => {
+    //     if(hasFocus) {
+    //         setTagSearch(tagSearch + event.key);
+    //         console.log(tagSearch);
+    //     }
+    // };
+
 
     useEffect(() => {
         
@@ -61,13 +125,51 @@ const TagInput = React.forwardRef((props, tagDisplay) => {
         return <div>Error loading tags</div>
     }
 
-    // data.tags = data.tags.sort(tagSorter);
 
     //clear tag search when component loses focus
-    !hasFocus && tagSearch.length && setTagSearch("");
+    // !hasFocus && tagSearch.length && setTagSearch("");
 
-    // console.log(data.tags.filter(filterTags));
 
+    return (
+        <div>
+            {/* where selected tags are displayed */}
+            <div className={tagDisplayClass}>
+                {
+                    selectedTags.map(tag => (
+                            <span key={tag.id} className={`${tagClass} is-family-primary`}>
+                                {tag.title}
+                                <span onClick={() => removeSelectedTag(tag.id)}>&#x274C;</span>
+                            </span>
+                        )
+                    )
+                }
+            </div>
+            {/* where tags are searched for */}
+            <input onChange={handleTagInputChange} ref={tagInputRef} value={tagInput} placeholder="Enter tags"/>
+            {/* where suggested tags are displayed */}
+            <div className={suggestedTags} ref={suggestedTagsRef}>
+                {
+
+                    data.tags
+                    .filter(filterTags)
+                    .sort(tagSorter)
+                    .map((tag) => (
+                            <p 
+                                key={tag.id} 
+                                tag_id={tag.id}
+                                tag_title={tag.title}
+                                onClick={() => addSelectedTag(tag)}
+                            >
+                                {tag.title}
+                            </p>
+                        )
+                    )
+                }
+            </div>
+        </div>
+    );
+
+    /*
      return (
         <div onKeyDown={handleKeyDown} tabIndex="0">
             <div className={tagDisplayClass} ref={tagDisplay}>
@@ -106,89 +208,7 @@ const TagInput = React.forwardRef((props, tagDisplay) => {
                 )
             }
         </div>
-    );
+    );*/
 });
 
-const TagInputHelpers = ({selectedTags, setSelectedTags}) => {
-    const tagSorter = (tag1, tag2) => {
-        const str1 = tag1.title.toLowerCase();
-        const str2 = tag2.title.toLowerCase();
-        const shorterStrLen = str1.length < str2.length ? str1.length : str2.length;
-      
-        for(let i = 0; i < shorterStrLen; i++) {
-          if(str1[i] !== str2[i]) {
-            return str1[i] < str2[i] ? -1 : 1
-          }
-        }
-        return str1.length < str2.length ? -1 : 1;
-    }
-    
-    const findSelectedTagIndex = tagId => {
-        return selectedTags.findIndex(selectedTag => {
-            return tagId === selectedTag.id;
-        });
-    }
-
-    const [hasFocus, setHasFocus] = useState();
-
-    //filter tags based on what user types
-    //clear tagSearch when user clicks off TagInput
-    const [tagSearch, setTagSearch] = useState("");
-
-    const tagDisplay = useRef(null);
-    
-    const addSelectedTag = e => {
-        //tag has not already been selected
-        const newTag = {title: e.target.getAttribute("tag_title"), id: e.target.getAttribute("tag_id")};
-        // const newTag = {title: e.target.innerHTML, id: e.target.getAttribute("tag_id")};
-
-        if(findSelectedTagIndex(newTag.id) === -1) 
-            setSelectedTags([...selectedTags, newTag]);
-    }
-
-    const removeSelectedTag = tagId => {
-        const index = findSelectedTagIndex(tagId);
-        setSelectedTags([...selectedTags.slice(0, index), ...selectedTags.slice(index + 1)]);
-    }
-        
-    const handleClick = event => {
-        setHasFocus(tagDisplay.current.contains(event.target));
-    };
-
-    const handleKeyDown = event => {
-        console.log(hasFocus);
-        if(hasFocus) {
-            setTagSearch(tagSearch + event.key);
-            console.log(tagSearch);
-        }
-    };
-
-    const filterTags = tag => {
-        if(tagSearch.length === 0)
-            return true;
-
-        return new RegExp(`\\b${tagSearch}`, "i").test(tag.title);
-        // return tag.title.toLowerCase().includes(tagSearch.toLowerCase());
-    };
-    
-    return (
-        <TagInput 
-            filterTags={filterTags} 
-            handleKeyDown={handleKeyDown}
-            handleClick={handleClick}
-            removeSelectedTag={removeSelectedTag}
-            addSelectedTag={addSelectedTag}
-            findSelectedTagIndex={findSelectedTagIndex}
-            tagSorter={tagSorter}
-            tagSearch={tagSearch}
-            setTagSearch={setTagSearch}
-            selectedTags={selectedTags}
-            setSelectedTags={setSelectedTags}
-            hasFocus={hasFocus}
-            ref={tagDisplay}
-            filterTags={filterTags}
-        />
-    )
-}
-
-export default TagInputHelpers;
+export default TagInput;
