5b09c680ef36e2d959d1c1c4d6b48a75a5813284
WStrellis <wstrellis@gmail.com>
2020-01-17 19:39:48 -0500

    basic geocoder works

--- src/components/geocoder/Geocoder.jsx
+++ src/components/geocoder/Geocoder.jsx
@@ -3,19 +3,58 @@ import {useCombobox} from 'downshift'
 import {fetchGeocode} from '../../utils'
 
 export default function Geocoder() {
-  const [search, setSearch] = useState('')
-  const handleChange = e => {
-    setSearch(e.target.value)
-  }
+  const [searchTerm, setSearchTerm] = useState('')
+  const [mbResults, setMbResults] = useState([])
+  let options = []
 
-  const getData = async () => {
-    let data = await fetchGeocode({searchWords: search})
-    console.log('data', data)
-  }
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getLabelProps,
+    getInputProps,
+    getMenuProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    items: mbResults,
+    // update list of options
+    onInputValueChange: async ({inputValue}) => {
+      console.log('inputValue', inputValue)
+      // fetch data from api
+      const data = await fetchGeocode({searchWords: inputValue})
+      console.log('data', data.features)
+      setMbResults(data.features.map(event => event.place_name))
+      // set options to array of strings
+      // options = mbResults.map(event => event.place_name)
+    },
+  })
+
   return (
     <>
-      <input type='text' onChange={handleChange} />
-      <button onClick={getData}>submit</button>
+      <label {...getLabelProps()}>Enter A Location</label>
+      <div {...getComboboxProps()}>
+        <input {...getInputProps()} />
+      </div>
+      <ul {...getMenuProps()}>
+        {isOpen &&
+          mbResults.map((place, indx) => (
+            <li
+              style={
+                highlightedIndex === indx ? {backgroundColor: 'yellow'} : {}
+              }
+              key={'place-' + indx}
+              // {...getItemProps({place, indx})}
+            >
+              {place}
+            </li>
+          ))}
+      </ul>
     </>
   )
 }
+
+// <>
+// <input type='text' onChange={handleChange} />
+// <button onClick={getData}>submit</button>
+// </>
