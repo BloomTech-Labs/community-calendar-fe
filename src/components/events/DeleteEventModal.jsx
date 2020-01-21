688d42bd171d1e536a4091189c7951a562d90be8
Mark King <markpkng@gmail.com>
2020-01-21 16:54:50 -0500

    added delete event modal

---
+++ src/components/events/DeleteEventModal.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+//styles
+import {
+    deleteEventModal,
+    deleteEventModalContent,
+    deleteEventModalBody,
+    deleteEventModalButton,
+    deleteEventModalButtonContainer,
+    yes,
+    no,
+    button
+  } from './styles/DeleteEventModal.module.scss'
+
+function DeleteEventModal({deleteEvent, toggleModal}) {
+    return (
+        <div className={`${deleteEventModal}`}id="modal">
+            <div className={`${deleteEventModalContent}`}>
+                <div className={`${deleteEventModalBody}`}>
+                    Are you sure you want to delete this event?
+                </div>
+            </div>
+            <div className={`${deleteEventModalButtonContainer}`}>
+                <button onClick={() => {deleteEvent()}} className={`${button} ${yes}`}>Yes</button>
+                <button onClick={() => {toggleModal()}} className={`${button} ${no}`}>No</button>
+            </div>
+        </div>
+    )
+}
+
+export default DeleteEventModal;
