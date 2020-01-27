1dfbef4dc2d2f4c54e66a95dfc68238fcbc7daa2
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-01-27 12:51:21 -0600

    Adds initial page styling
    Incorporates gear icon for user settings

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+//  Components
+import GearIcon from 'icons/GearIcon'
+
+
+
+import {
+  userProfile,
+  profileInfo,
+  userEvents,
+  userInfo
+} from "./styles/UserProfile.module.scss"
 
 
 const UserProfile = () => {
   return (
-    <div className="userProfile">
-      <p>User Profile</p>
+    <div className={userProfile}>
+      <div className={profileInfo}>
+        <GearIcon />
+        <div className={userInfo}>
+        
+        </div>
+      </div>
+      <div className={userEvents}>
+        <p>User Events</p>
+      </div>
     </div>
   )
 }
