db3ccdf953469964381e0d3be8cdfd70ad925287
lowell1 <lowelljacobs@mail.com>
2020-01-30 14:06:29 -0800

    added buttons for selecting our mission or leadership

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 import {
     banner,
-    container
+    container,
+    buttonContainer
 } from "./styles/AboutUs.module.scss"
 
+const ourMission = 1;
+const leadership = 2;
+
 const AboutUs = () => {
+    //if selectedPage = ourMission display our mission component
+    //if selectedPage = leadership display leadership component
+    const [selectedPage, setSelectedPage] = useState(ourMission);
+
+    console.log(selectedPage);
+
     return (
         <div className={container}>
             <div className={banner}>
                 <h1>More than just events</h1>
+            </div>
+            <div className={buttonContainer}>
+                {/* buttons for selecting which "page" to display */}
+                <div style={{borderBottom: selectedPage === ourMission ? "1px solid red" : "none"}} onClick={() => setSelectedPage(ourMission)}><span>OUR MISSION</span></div>
+                <div style={{borderBottom: selectedPage === leadership ? "1px solid red" : "none"}} onClick={() => setSelectedPage(leadership)}><span>LEADERSHIP</span></div>
             </div>
         </div>
     )
