9731aa4a539a68b31865e92be7df5948045d2327
gelinas <jlgelinas@gmail.com>
2020-04-29 23:29:42 -0400

    event form loads

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import {onError} from 'apollo-link-error'
 import Home from './pages/Home'
 import EventView from './pages/EventView'
 // import UpdateEventPage from './pages/UpdateEventPage'
-// import CreateEventPage from './pages/CreateEventPage'
+import CreateEventPage from './pages/CreateEventPage'
 import SearchResults from './pages/SearchResults'
 // import UserProfile from './pages/UserProfilePage'
 import AboutUs from './pages/AboutUs'
@@ -137,7 +137,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path='/' component={Home} />
-        {/* <Route path='/create-event' component={CreateEventPage} /> */}
+        <Route path='/create-event' component={CreateEventPage} />
         {/* <Route path='/myprofile' component={UserProfile} /> */}
         <Route exact path='/events/:id' component={EventView} />
         {/* <Route exact path='/events/:id/update' component={UpdateEventPage} /> */}
