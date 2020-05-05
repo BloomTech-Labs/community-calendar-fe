5f4f2a364cbd8467e567bad5ad2349c873e6d993
Josue Rodriguez <jr2589213@gmail.com>
2020-05-05 10:19:27 -0700

    removed unwanted logic from the previous homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -100,102 +100,10 @@ const Home = () => {
   return (
     <div className='page-wrapper'>
       {/* Featured Events carousel */}
-      {featuredApolloData.data && featuredApolloData.data.events.length > 0 ? (
-        <>
-          <FeatCarousel apolloData={featuredApolloData} />
-          <div className='content-divider-x'></div>
-        </>
-      ) : null}
-
-      {/* Events list */}
-      <section className='section mobile-section'>
-        <div className='is-flex level justify-between is-dark '>
-          <h3 className='is-family-secondary is-size-3-mobile is-size-2-tablet has-text-black-bis'>
-            Events
-          </h3>
-          {localCache.userLatitude && localCache.userLongitude && (
-            <DistanceDropdown
-              client={client}
-              userLat={localCache.userLatitude}
-              userLong={localCache.userLongitude}
-              maxDistance={localCache.maxDistance}
-            />
-          )}
-        </div>
-        <FilterBtns
-          refetch={refetch}
-          eventRange={eventRange}
-          setEventRange={setEventRange}
-          start={start}
-          setStart={setStart}
-          end={end}
-          setEnd={setEnd}
-          setDatePickerIsOpen={setDatePickerIsOpen}
-        />
-        <div className='is-flex justify-between'>
-          <div
-            className={`dropdown  navDropdown ${
-              datePickerIsOpen ? 'is-active' : ''
-            }`}
-          >
-            <div
-              role='button'
-              className='dropdown-trigger level is-clickable hover-underline is-size-7-mobile'
-              aria-haspopup='true'
-              aria-controls='dropdown-menu2'
-              data-testid='location-dropdown-trigger'
-              data-id='location-dropdown'
-              onClick={() => setDatePickerIsOpen(!datePickerIsOpen)}
-            >
-              <span
-                className={` is-size-5-tablet no-outline-focus no-pointer-events`}
-              >
-                {start && end
-                  ? Math.ceil(
-                      moment.duration(moment(end).diff(moment(start))).asDays(),
-                    ) === 1
-                    ? moment(start).format('ddd, MMM Do YYYY')
-                    : `${moment(start).format('ddd, MMM Do YYYY')} - ${moment(
-                        end,
-                      ).format('ddd, MMM Do YYYY')}`
-                  : 'Select a date range'}
-              </span>
-              <span
-                className={`${
-                  datePickerIsOpen ? 'flip' : ''
-                } no-pointer-events icon`}
-                style={{transition: 'transform 0.2s'}}
-              >
-                <DropdownIcon />
-              </span>
-            </div>
-            <div
-              className={` dropdown-menu drop-center has-background-white border-radius`}
-              id='location-dropdown-menu '
-              role='menu'
-              style={{
-                border: '1px solid #21242c',
-                position: 'absolute',
-                top: '32px',
-              }}
-            >
-              <SelectedRange
-                refetch={refetch}
-                setStart={setStart}
-                setEnd={setEnd}
-                setEventRange={setEventRange}
-              />
-            </div>
-            {/* end dropdown-menu*/}
-          </div>
-          <ViewToggle toggleFunc={setShowListView} viewState={useListView} />
-        </div>
-        <EventList
-          apolloData={{data: data && data.events, loading, error}}
-          listView={useListView}
-          setListType={setShowListView}
-        />
-      </section>
+      <>
+        <FeatCarousel apolloData={featuredApolloData} />
+        <div className='content-divider-x'></div>
+      </>
     </div>
   )
 }
