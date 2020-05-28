4eb73c97f959d2da12f107f402714345e18c537e
Josue Rodriguez <jr2589213@gmail.com>
2020-05-28 12:48:44 -0700

    fixes error fileErrors -> fileType

--- src/components/event_forms/EventForm.jsx
+++ src/components/event_forms/EventForm.jsx
@@ -362,7 +362,7 @@ const EventForm = (props) => {
   }
 
   // if user attempt to submit empty form throw error
-  if (Object.keys(formErrors).length && !fileUpload && formErrors === 'add') {
+  if (Object.keys(formErrors).length && !fileUpload && formType === 'add') {
     if (!error) {
       appendErrorOnImageUploader()
     }
